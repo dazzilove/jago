caefa3d8b35fe78b594eb36f7ea2d634aeb2b9d8
dazzilove <dazzilove@gmail.com>
2018-07-09 23:17:56 +0900

    db connect

--- jago-node-api/jago-server-3247.js
+++ jago-node-api/jago-server-3247.js
@@ -1,3 +1,11 @@
+const NeDB = require('nedb')
+const path = require('path')
+
+const db = new NeDB({
+    filename: path.join(__dirname, 'jago.db'),
+    autoload: true
+})
+
 const express = require('express')
 const portNo = 3000
 const app = express()
